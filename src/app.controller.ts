d41d167418b9a56b31de5be405b9b449321b1040
ljq0226 <1035821043@qq.com>
2022-10-12 14:54:18 +0800

    setup prisma

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,7 @@ import { AppService } from './app.service';
 import { ParseIntPipe } from './core/pipe/parse-int.pipe';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
+  constructor(private readonly appService: AppService) {}
 
   @Get(':id')
   getHello(@Param('id', new ParseIntPipe()) id: number): string {
