49a825078f8da723d7963637d2434b605c433ee8
ljq0226 <1035821043@qq.com>
2022-10-13 13:57:14 +0800

    install configModule

--- src/core/pipe/parse-int.pipe.ts
+++ src/core/pipe/parse-int.pipe.ts
@@ -9,9 +9,7 @@ import {
 export class ParseIntPipe implements PipeTransform<string, number> {
   transform(value: string, metadata: ArgumentMetadata): number {
     const val = parseInt(value, 10);
-    console.log(metadata.type);
-    console.log(metadata.metatype);
-    console.log(metadata.data);
+    console.log('111');
     if (isNaN(val)) {
       console.log('参数类型错误');
       throw new BadRequestException('Validation failed');
